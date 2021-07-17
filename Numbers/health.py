c602f6a7e785f9977eef4c9e8e16c0f32f78bf72
anahitahassan <anahitarhassan@gmail.com>
2021-07-17 13:38:56 -0700

    random.randint

--- Numbers/health.py
+++ Numbers/health.py
@@ -1,2 +1,11 @@
 # 15: PROJECT 1: Crafting a Health Potion - Part 1
 # -------------------------------------------------------------
+
+import random
+health = 50 
+
+# Note: search up 'Python docs Module Index' to read documentation. 
+# Alphabetical list of all modules: https://docs.python.org/3/py-modindex.html
+# Go to r -> random -> random.randint(a, b)
+# "Return a random integer N such that a <= N <= b."
+
