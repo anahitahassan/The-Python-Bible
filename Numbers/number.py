446a78c16818f20b54ef20c88936787c7526253d
anahitahassan <anahitarhassan@gmail.com>
2021-07-17 13:23:50 -0700

    comments

---
+++ Numbers/number.py
@@ -0,0 +1,6 @@
+# Videos 12-15 
+# Topics: control order of computations, learn about floats, using External Python Modules
+# First project: make a ehalth potion for a video game 
+# Python math module: rounding, powers, square roots, basic trig, more advanced topics
+
+# 13: Basic Arithmetic, Floats and Modulo
