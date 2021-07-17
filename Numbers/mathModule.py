f76587c927e89d5ac7127be69bf7525075a58154
anahitahassan <anahitarhassan@gmail.com>
2021-07-17 13:54:37 -0700

    constants

---
+++ Numbers/mathModule.py
@@ -0,0 +1,18 @@
+# 17: Fun with the Python Math Module
+# Documentation: https://docs.python.org/3/library/math.html#module-math
+
+# Round function
+print(round(2.1)) # 2
+print(round(1.5)) # 2 -> what if you wanted .5 to round down?
+print(round(1.7)) # 2
+
+import math
+print(math.floor(1.5)) # now it's 1!
+print(math.ceil(3.1)) # now it's 4!
+
+# Constants (prints 15 digits after decimal)
+print(math.pi) 
+print(math.e) 
+print(math.tau) 
+
+
