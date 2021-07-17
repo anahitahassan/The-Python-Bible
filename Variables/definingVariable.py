ee4a3f5980a802c321dbf4a6c5244bb2af71faa5
anahitahassan <anahitarhassan@gmail.com>
2021-07-16 20:09:23 -0700

    spacing

--- Variables/definingVariable.py
+++ Variables/definingVariable.py
@@ -1,8 +1,10 @@
 # Last section, we made our first python script, but how can we save the results? 
 # Variables all have a uniquely identifiable name. 
 # A Variables has a name and a certain value. 
+
 number = 12
 print(number)
+
 # When I tried to run this, I ran into an error. It was cause I had ")" in my folder name "1)Variables"
 # Now I know not to use ")" and other similar symbols in folder/file names. 
 
@@ -11,12 +13,16 @@ print(number)
 
 # Python is a dynamically typed language
 # Each variable can have a certain type and you can find that by using what's called the type function. 
+
 print(type(number))
+
 # '<class 'int'>' is printed. 
 # The type of our variables can change
 # This is different from statically typed languages like C or Java,
 # In Java you need to define the type of variable when you create it; in Python you dont!
+
 number = "hello"
 print(type(number))
+
 # Now it says: <class 'str'>, as expected. 
 
--- Variables/updatingOurScript.py
+++ Variables/updatingOurScript.py
@@ -1,8 +1,11 @@
 # Note: The instructor of the course works directly on shell, but I'd rather use notebook
+
 first_number = 1+1
 second_number = 105+10
 print(first_number)
 print(second_number)
+
 # Variable names cannot start with a number. 
+
 total = first_number + second_number
 print(total)
