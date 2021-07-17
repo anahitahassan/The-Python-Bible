6f93f00acbae72b954e7017e1844c082ce7fae2d
anahitahassan <anahitarhassan@gmail.com>
2021-07-17 14:03:26 -0700

    last comment

--- Numbers/mathModule.py
+++ Numbers/mathModule.py
@@ -23,8 +23,19 @@ print(math.sin( math.pi )) # not exactly 0, but if we wanted 0:
 print(math.floor( math.sin( math.pi ) ) )
 
 # Hypotenuse
-print( math.hypot(3, 4))
-print( math.hypot(5, 12))
+print( math.hypot(3, 4) )
+print( math.hypot(5, 12) )
 
-# Power
-print( math.pow(2, 3))
+# Power 
+print( math.pow(2, 3) )
+# Shortcut: 2 ** 3 = 8. 
+
+# Exp and Log
+print (math.exp(2) ) 
+# math.exp(x): Return e raised to the power x, where e = 2.718281… is the base of natural logarithms.
+# This is usually more accurate than math.e ** x or pow(math.e, x).
+print( math.log(math.e) ) # recall that math.e is the constant e!
+print( math.log10(1000) ) # we get 3 since 10 to power of 3 is 1000. 
+print(math.log2(8))
+
+# check out NumPy and SciPi libraries for more advanced math tools!
