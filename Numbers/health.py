d1e7a0378b40d3ae61d28befb37a59cf69307993
anahitahassan <anahitarhassan@gmail.com>
2021-07-17 13:48:43 -0700

    casting

--- Numbers/health.py
+++ Numbers/health.py
@@ -3,13 +3,21 @@
 
 import random
 health = 50 
+difficulty = 1 # where hard = 1, medium = 2, easy = 3
+
 
 # Note: search up 'Python docs Module Index' to read documentation. 
 # Alphabetical list of all modules: https://docs.python.org/3/py-modindex.html
 # Go to r -> random -> random.randint(a, b)
 # "Return a random integer N such that a <= N <= b."
-print(random.randint(10, 20))
+random.randint(10, 20)
 
-potion_health = random.randint(25, 50)
+potion_health = int( random.randint(25, 50) / difficulty) # dividing by low number makes big number. 
 # We imported the random *module* and used the *function* randint. 
 
+health = health + potion_health
+print(health)
+
+# As in Java, you can use += to change one variable by adding another variable to it. 
+
+# Casting (putting int and brackets around some code)
