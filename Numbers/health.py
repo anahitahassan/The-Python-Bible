1c7d76fbe43b3034d82b208311aa2680668067e1
anahitahassan <anahitarhassan@gmail.com>
2021-07-17 13:42:02 -0700

    made potion health random var

--- Numbers/health.py
+++ Numbers/health.py
@@ -8,4 +8,8 @@ health = 50
 # Alphabetical list of all modules: https://docs.python.org/3/py-modindex.html
 # Go to r -> random -> random.randint(a, b)
 # "Return a random integer N such that a <= N <= b."
+print(random.randint(10, 20))
+
+potion_health = random.randint(25, 50)
+# We imported the random *module* and used the *function* randint. 
 
