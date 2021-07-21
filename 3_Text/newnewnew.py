6a9c7569ada2d14d545963a70e430eb13362f3c7
Anahita Hassan <anahitarhassan@gmail.com>
2021-07-20 18:36:19 -0700

    recent updates

---
+++ 3_Text/newnewnew.py
@@ -0,0 +1,31 @@
+# 21: PROJECT 2: Hello You! - Part 1 - Project Overview
+
+# Ask user for name
+# Create output text
+# Print output to screen
+
+# Search up Python docs
+# Go to Python Standard Library
+# Look for built in functions, read the documentation!
+# This is a lot different from Java where you need scanner
+
+s = input('-->') # If I input 'Coding is super fun'...
+print(s)         # Then, 'Coding is super fun' is printed since it was saved as 's'. 
+
+name = input("What is your name?:")
+print(name)
+
+# Ask user for age
+age = input("What is your age?:")
+print(age)
+
+# Ask user for city
+city = input("What is your city?:")
+print(city)
+
+# Ask user what they enjoy
+enjoy = input("What do you enjoy?:")
+print(enjoy)
+
+# NOTE: Say you said '23' for the age question
+# when you do type() you'll find that it was saved as a string.
