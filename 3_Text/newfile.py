71e4899fc32947132c9dbfd87afd6f03aa01c630
Anahita Hassan <anahitarhassan@gmail.com>
2021-07-20 18:20:40 -0700

    new

---
+++ 3_Text/newfile.py
@@ -0,0 +1,9 @@
+# 19: Section Overview
+#   - Create "Hello World" Program
+#   - User input
+#   - String concatenation
+#   - Customize text with variables using String Formating
+#   - Inbuilt tools for working with strings in Python
+#   - Email slicer project
+
+
