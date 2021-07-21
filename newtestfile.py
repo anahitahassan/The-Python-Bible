0c9837880ca268ae0d9c14d2b6d0461cd83220ef
Anahita Hassan <anahitarhassan@gmail.com>
2021-07-20 18:16:17 -0700

    new updates

--- newtestfile.py
+++
@@ -1,6 +0,0 @@
-number = 1
-while number < 10:
-    print("This is a test")
-    number = number + 1
-
-#most upudated code
--- testfile.py
+++ testfile.py
@@ -1,2 +1,9 @@
 phrase = "This is a test file. "
 print(phrase)
+
+number = 1
+while number < 10:
+    print("This is a test")
+    number = number + 1
+
+#most upudated code
