f409a3bd39a368e07fcf8f815a20a6e48b431d7a
Anahita Hassan <anahitarhassan@gmail.com>
2021-07-20 18:25:44 -0700

    new

--- 3_Text/newfile.py
+++ 3_Text/newfile.py
@@ -6,4 +6,27 @@
 #   - Inbuilt tools for working with strings in Python
 #   - Email slicer project
 
+# 20: Hello World
+
+hello = "Hello world!"
+print(hello)
+
+name = "Anahita"
+print( type(name) )
+
+# Be careful when using quotes inside the string:
+message = 'John said to me " I will see you later " '
+
+# You can use triple quotes (""") or triple single quotes (''') when you have lines of text. 
+romeo = """Alas, that love, whose view is muffled still,
+Should, without eyes, see pathways to his will!
+Where shall we dine? O me! What fray was here?
+Yet tell me not, for I have heard it all.
+Here’s much to do with hate, but more with love.
+Why, then, O brawling love! O loving hate!
+O any thing, of nothing first create!
+O heavy lightness! serious vanity!
+Misshapen chaos of well-seeming forms!
+Feather of lead, bright smoke, cold fire,
+sick health!"""
 
