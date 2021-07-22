48f4e51b72bdd9f16523ac345ae41ea469ecfc22
Anahita Hassan <anahitarhassan@gmail.com>
2021-07-21 20:14:59 -0700

    new file

---
+++ 4_Logic/booleans.py
@@ -0,0 +1,22 @@
+# 30: Section Overview
+#   - Boolean Datatype
+#   - Conditional Operators
+#   - If / if-else statements
+#   - Logical Keywords
+
+# 31: Booleans & Comparison Operators
+d = True
+e = "True"
+print(type(d)) # <class 'bool'>
+print(type(e)) # <class 'str'>
+
+print(2 > 3)
+print(3 > 2)
+print(type(2 < 3))
+
+# note: you can't use '=', it must be '=='
+print(2 == 3)
+print(3 == 3)
+
+print(4 >= 3)
+print(2 >= 3)
