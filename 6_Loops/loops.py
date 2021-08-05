08b650ebb411a40c735500edcc64e998ea7822e6
Anahita Hassan <anahitarhassan@gmail.com>
2021-08-05 09:49:58 -0700

    new

--- 6_Loops/loops.py
+++ 6_Loops/loops.py
@@ -5,3 +5,25 @@
 
 # 48: While Loops
 
+# Example 1:
+while False:
+    print("Hello")
+
+# Example 2:
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+# Example 3:
+list = []
+while len(list) < 3:
+    new_name = input("Please add a new name: ").strip().capitalize()
+    list.append(new_name)
+
+print("Sorry list is full")
+print(list)
+
+
+# 49: Project 6: Baby Conversation Simulator
+# NEWEST.
