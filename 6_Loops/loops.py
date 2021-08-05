ae0bcbac01832092fd8a1ce9b442d0d937d32430
Anahita Hassan <anahitarhassan@gmail.com>
2021-08-05 09:54:15 -0700

    from random import choice

--- 6_Loops/loops.py
+++ 6_Loops/loops.py
@@ -26,4 +26,13 @@ print(list)
 
 
 # 49: Project 6: Baby Conversation Simulator
-# NEWEST.
+
+from random import choice
+
+questions = ["Why is the sky blue?", "Why is there a face on the moon?", "Where are the dinosaurs?"]
+
+question = choice(questions)
+answer = input(question).strip().lower()
+
+while answer != "just because":
+    answer = input("Why?: ").strip().lower()
