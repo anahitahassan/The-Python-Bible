410ff214f8961f3a79232b040e7be10d3a864156
Anahita Hassan <anahitarhassan@gmail.com>
2021-08-06 10:31:20 -0700

    overview

---
+++ 8_Object_Oriented_Programming/overview.py
@@ -0,0 +1,7 @@
+# 65: Section Overview
+#   - Learn about objects and classes
+#   - Learn how to use class variables and class methods
+#   - Learn about class constructors and destructors, class variable and class methods
+#   - Model a Coin
+#   - Model a Set of Coins
+#   - BUild your own Bank
