75ff0eab3e56c3aeb829d10fb8e625fdafd297e6
Anahita Hassan <anahitarhassan@gmail.com>
2021-08-06 09:28:28 -0700

    functions

---
+++ 7_Functions/functions.py
@@ -0,0 +1,14 @@
+# 56: Intro to Functions
+
+# 57: What are Functions?
+
+def add(x, y):
+    return (x+y)
+
+print(add(11, 4))
+
+answer = add(100, 20)
+print(type(answer)) 
+# <class 'int'>
+
+# 58: Variable Scope
