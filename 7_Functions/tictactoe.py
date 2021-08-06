72bc6956c2e4b68c158be0688b8dbad96e1b38c3
Anahita Hassan <anahitarhassan@gmail.com>
2021-08-06 10:29:38 -0700

    review of functions

--- 7_Functions/tictactoe.py
+++ 7_Functions/tictactoe.py
@@ -1,2 +1,85 @@
-# 62: Project 8: Tic Tac Toe
+# 62/63: Project 8: Tic Tac Toe
 
+# the data structure we're using is list not tuple since we need the board to be dynamic
+
+board = [" " for i in range(9)]
+
+def print_board():
+    row1 = "| {} | {} | {} |".format(board[0], board[1], board[2])
+    row2 = "| {} | {} | {} |".format(board[3], board[4], board[5])
+    row3 = "| {} | {} | {} |".format(board[6], board[7], board[8])
+
+    print()
+    print(row1)
+    print(row2)
+    print(row3)
+    print()
+
+def player_move(icon):
+
+    if icon == "X":
+        number = 1
+    elif icon == "O":
+        number = 2
+
+    print("Your turn player {}.".format(number))
+
+    choice = int(input("Enter your move (1-9): ").strip())
+    if board[choice - 1] == " ":
+        board[choice - 1] = icon
+    else:
+        print()
+        print("That space is taken!")
+
+    
+def is_Victory(icon): 
+    if (board[0] == icon and board[1] == icon and board[2] == icon) or \
+        (board[3] == icon and board[4] == icon and board[5] == icon) or \
+        (board[6] == icon and board[7] == icon and board[8] == icon) or \
+        (board[0] == icon and board[3] == icon and board[6] == icon) or \
+        (board[1] == icon and board[4] == icon and board[7] == icon) or \
+        (board[2] == icon and board[5] == icon and board[8] == icon) or \
+        (board[0] == icon and board[4] == icon and board[8] == icon) or \
+        (board[3] == icon and board[5] == icon and board[6] == icon):
+        return True
+    else:
+        return False
+        
+def is_draw():
+    if " " not in board:
+        return True
+    else: 
+        return False
+
+
+while True:
+    print_board()
+    player_move("X")
+    print_board()
+
+    if is_Victory("X"):
+        print("X Wins! Congrats!")
+        break
+    elif is_draw():
+        print("It's a draw!")
+        break
+    player_move("O")
+    if is_Victory("O"):
+        print("O Wins! Congrats!")
+        break
+    
+# how to reset the game????
+
+#   64: Section Review
+#       - Learned what functions are and how to create them
+#       - Learned the difference between returning values and printing to screen
+#       - Learned about Variable Scope
+#       - Learned the difference between arguments and parameters
+#       - Learned how to pack and unpack args and kwargs using * and ** respectively. 
+    
+
+    
+    
+
+    
+    
