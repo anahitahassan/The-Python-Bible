e5f3238edab3a01f36d50a3c9ec2869bb67a7a7c
Anahita Hassan <anahitarhassan@gmail.com>
2021-08-06 09:55:40 -0700

    unpacking

--- 7_Functions/keyword_args_default_parameters.py
+++ 7_Functions/keyword_args_default_parameters.py
@@ -1,2 +1,24 @@
 # 60: Keyword Arguments and Default Parameters
 
+def about(name, age, likes):
+    sentence = "Meet {}! They are {} years old and they like {}.".format(name, age, likes)
+    return sentence
+
+print(about("Anahita", 18, "Python"))
+
+print(about(age = 18, name = "Anahita", likes = "Python"))
+
+# print(about("Anahita", 18)) This won't work!
+
+def new_about(name, age, likes = "teaching"):
+    sentence = "Meet {}! They are {} years old and they like {}.".format(name, age, likes)
+    return sentence
+
+print(new_about(name = "Ziyad", age = 29))
+
+
+# def new_about(name, age = 29, likes):
+#     sentence = "Meet {}! They are {} years old and they like {}.".format(name, age, likes)
+#     return sentence
+
+# This doesn't work because it's a non-default argument (likes) after a default argument (age = 29). 
---
+++ 7_Functions/packing_unpacking.py
@@ -0,0 +1,36 @@
+# 61: Packing and Unpacking using *args and *kwargs
+
+# Unpacking: 
+
+print(1, 2, 3, 4, 5)
+# 1 2 3 4 5
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+# [1, 2, 3, 4, 5]
+
+print(*numbers)
+# 1 2 3 4 5
+
+print("abc") # abc
+print(*"abc") # a b c
+
+def add(x, y):
+    return x + y
+
+print(add(10, 10))
+
+def add(*numbers):
+    total = 0
+    for number in numbers:
+        total = total + number
+    return(total)
+
+print(add(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+def about(name, age, likes):
+    sentence = "Meet {}! They are {} years old and they like{}.".format(name, age, likes)
+    return sentence
+
+dictionary = {"name": "Ziyad", "age": 23, "likes": "Python"}
+print(about(**dictionary))
