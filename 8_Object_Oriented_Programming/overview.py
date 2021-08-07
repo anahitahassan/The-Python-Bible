e2f19f5ba04c54e24e30cac902c82163db6e21a9
Anahita Hassan <anahitarhassan@gmail.com>
2021-08-06 17:42:09 -0700

    made class pound

--- 8_Object_Oriented_Programming/overview.py
+++ 8_Object_Oriented_Programming/overview.py
@@ -12,3 +12,34 @@
 # Classes are made of States and Methods
 # States (of a coin): values, color, diameter, heads
 # Methods (of a coin): flip() 
+
+# 67: Project 9: Make your own Coin 
+class Pound: 
+
+    # States
+    value = 1.00
+    color = "gold"
+    num_edges = 1
+    diameter = 22.5
+    thickness = 3.15
+    heads = True
+
+# out first object/instance
+coin1 = Pound()
+print(type(coin1))
+# <class '__main__.Pound'>
+
+print(coin1.value)
+
+coin1.color = "greenish"
+print(coin1.color)
+
+coin2 = Pound()
+print(coin2.color)
+
+# notice how the color didn't change this time?
+# it's cause coin2 was made new from the Pound template. 
+# each object is independent, even though they came form the same class. 
+
+# 
+
